62197d078ba540a8c92883d546de5a3f08211f5b
iplayback <billy.li.x@gmail.com>
2016-07-16 17:11:41 +1000

    Created a list of pokemon and allow name editing

--- angular2-quickstart/app/app.component.js
+++ angular2-quickstart/app/app.component.js
@@ -11,15 +11,39 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 var core_1 = require('@angular/core');
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'List of Pokemon';
+        this.pokemons = POKEMON;
     }
+    AppComponent.prototype.onSelect = function (pokemon) {
+        this.selectedPokemon = pokemon;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular 2 App!</h1>'
+            template: "\n    <h1>{{title}}</h1>\n    <h2>My Pokemons</h2>\n    <ul class=\"pokemons\">\n      <li *ngFor=\"let pokemon of pokemons\"\n        [class.selected]=\"pokemon === selectedPokemon\"\n        (click)=\"onSelect(pokemon)\">\n        <span class=\"badge\">{{pokemon.id}}</span> {{pokemon.name}}\n      </li>\n    </ul>\n    <div *ngIf=\"selectedPokemon\">\n      <h2>{{selectedPokemon.name}} details!</h2>\n      <div><label>id: </label>{{selectedPokemon.id}}</div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"selectedPokemon.name\" placeholder=\"name\"/>\n      </div>\n    </div>",
+            styles: ["\n    .selected {\n      background-color: #CFD8DC !important;\n      color: white;\n    }\n    .pokemons {\n      margin: 0 0 2em 0;\n      list-style-type: none;\n      padding: 0;\n      width: 15em;\n    }\n    .pokemons li {\n      cursor: pointer;\n      position: relative;\n      left: 0;\n      background-color: #EEE;\n      margin: .5em;\n      padding: .3em 0;\n      height: 1.6em;\n      border-radius: 4px;\n    }\n    .pokemons li.selected:hover {\n      background-color: #BBD8DC !important;\n      color: white;\n    }\n    .pokemons li:hover {\n      color: #607D8B;\n      background-color: #DDD;\n      left: .1em;\n    }\n    .pokemons .text {\n      position: relative;\n      top: -3px;\n    }\n    .pokemons .badge {\n      display: inline-block;\n      font-size: small;\n      color: white;\n      padding: 0.8em 0.7em 0 0.7em;\n      background-color: #607D8B;\n      line-height: 1em;\n      position: relative;\n      left: -1px;\n      top: -4px;\n      height: 1.8em;\n      margin-right: .8em;\n      border-radius: 4px 0 0 4px;\n    }\n  "]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
     return AppComponent;
 }());
 exports.AppComponent = AppComponent;
+var Pokemon = (function () {
+    function Pokemon() {
+    }
+    return Pokemon;
+}());
+exports.Pokemon = Pokemon;
+var POKEMON = [
+    { id: 11, name: 'Bulbasuar' },
+    { id: 12, name: 'Ivysaur' },
+    { id: 13, name: 'Venosaur' },
+    { id: 14, name: 'Charmander' },
+    { id: 15, name: 'Charmeleon' },
+    { id: 16, name: 'Charizard' },
+    { id: 17, name: 'Squirtle' },
+    { id: 18, name: 'Wartortle' },
+    { id: 19, name: 'Blastoise' },
+    { id: 20, name: 'Dragonite' }
+];
 //# sourceMappingURL=app.component.js.map
--- angular2-quickstart/app/app.component.ts
+++ angular2-quickstart/app/app.component.ts
@@ -1,6 +1,100 @@
 import { Component } from '@angular/core';
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App!</h1>'
+    template:`
+    <h1>{{title}}</h1>
+    <h2>My Pokemons</h2>
+    <ul class="pokemons">
+      <li *ngFor="let pokemon of pokemons"
+        [class.selected]="pokemon === selectedPokemon"
+        (click)="onSelect(pokemon)">
+        <span class="badge">{{pokemon.id}}</span> {{pokemon.name}}
+      </li>
+    </ul>
+    <div *ngIf="selectedPokemon">
+      <h2>{{selectedPokemon.name}} details!</h2>
+      <div><label>id: </label>{{selectedPokemon.id}}</div>
+      <div>
+        <label>name: </label>
+        <input [(ngModel)]="selectedPokemon.name" placeholder="name"/>
+      </div>
+    </div>`,
+    styles: [`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .pokemons {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 15em;
+    }
+    .pokemons li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .pokemons li.selected:hover {
+      background-color: #BBD8DC !important;
+      color: white;
+    }
+    .pokemons li:hover {
+      color: #607D8B;
+      background-color: #DDD;
+      left: .1em;
+    }
+    .pokemons .text {
+      position: relative;
+      top: -3px;
+    }
+    .pokemons .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0 0 4px;
+    }
+  `]
 })
-export class AppComponent { }
+
+export class AppComponent {
+
+    title = 'List of Pokemon';
+    pokemons = POKEMON;
+    selectedPokemon: Pokemon;
+
+    onSelect(pokemon: Pokemon) {
+        this.selectedPokemon = pokemon;
+    }
+}
+
+export class Pokemon {
+    id: number;
+    name: string;
+}
+
+const POKEMON: Pokemon[] = [
+    { id: 11, name: 'Bulbasuar' },
+    { id: 12, name: 'Ivysaur' },
+    { id: 13, name: 'Venosaur' },
+    { id: 14, name: 'Charmander' },
+    { id: 15, name: 'Charmeleon' },
+    { id: 16, name: 'Charizard' },
+    { id: 17, name: 'Squirtle' },
+    { id: 18, name: 'Wartortle' },
+    { id: 19, name: 'Blastoise' },
+    { id: 20, name: 'Dragonite' }
+];
